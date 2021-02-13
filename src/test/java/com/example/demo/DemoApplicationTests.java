44d82145100ab1073e9fca70053270f4b1cdec5b
GregoryFilipson <alivenemo@gmail.com>
2021-02-13 11:52:52 +0300

    added dockerfiles

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
+	public static GenericContainer<?> devapp = new GenericContainer<>("devapp");
+	public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp");
+
+	@BeforeAll
+	public static void setUp() {
+		devapp.start();
+		prodapp.start();
+	}
+
+	@Test
+	void contextLoads() {
+		ResponseEntity<String> forEntity_02 = restTemplate.getForEntity("http://localhost:"
+				+ devapp.getMappedPort(8080), String.class);
+		ResponseEntity<String> forEntity_01 = restTemplate.getForEntity("http://localhost:"
+				+ prodapp.getMappedPort(8081), String.class);
+		assertEquals("Current profile is dev", forEntity_02.getBody());
+		assertEquals("Current profile is production", forEntity_01.getBody());
+
+	}
+
+}
